a59ac18dec1bc0dc487cc39d6c5ecf0e88c3276a
Felipe Munhoz <felipe.munhoz@oncast.com.br>
2011-06-09 10:36:45 -0300

    Created post install script to put fpm on path.

---
+++ examples/fpm/postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s /opt/ruby/bin/ruby /usr/bin/ruby
+ln -s /opt/ruby/bin/irb /usr/bin/irb
+ln -s /opt/ruby/bin/rake /usr/bin/rake
+ln -s /opt/ruby/bin/erb /usr/bin/erb
+ln -s /opt/ruby/bin/gem /usr/bin/gem
+ln -s /opt/ruby/bin/rdoc /usr/bin/rdoc
+ln -s /opt/ruby/bin/ri /usr/bin/ri
+ln -s /opt/ruby/bin/testrb /usr/bin/testrb
+ln -s /opt/ruby/bin/bundle /usr/bin/bundle
